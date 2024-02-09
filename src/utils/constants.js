b2b59012357d5a5bb43fa2276f59c7a382b58eb9
Biplab Mahato <bips4lcs@gmail.com>
2024-02-09 17:16:05 +0530

    Update constants.js
    API key issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,9 @@
-const GOOGLE_API_KEY = "AIzaSyDD6Nlu_7gyz3G5o19h9w3wEWZBAwU8e7A";
+const GOOGLE_API_KEY = "";
 const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+ GOOGLE_API_KEY;
 export default YOUTUBE_VIDEO_API;
 
 export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
 
 export const OFFSET_LIVCHAT = 25;
+
+//#$#AIzaSyDD6Nlu_7gyz3G5o#$#19h9w3wEWZBAwU8e7A#$#
